9efe354f4fee0483085de0eca0d28d8f196ea435
Devi Sridharan <dsridharan@linkedin.com>
2018-06-22 17:35:20 -0700

    Initial setup

---
+++ app/resources/class2.java
@@ -0,0 +1,7 @@
+package resources;
+
+public class class2 {
+    public static void main(String args){
+      System.out.print("Class 2");
+  }
+}
---
+++ app/resources/prototype/class3.java
@@ -0,0 +1,7 @@
+package resources.prototype;
+
+public class class3 {
+    public static void main(String args){
+      System.out.print("Class 1");
+  }
+}
